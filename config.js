1fcbdffacc156507e4f56d43dd6ee18f395ad634
wportillomartinez881-sketch <wportillomartinez881@gmail.com>
2026-08-26 06:24:27 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
  * Configuración global del sistema NEXUS
  */
 const CONFIG = {
-    // URL oficial de la Web App desplegada en Google Apps Script
-    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzoCo98Z-eYWrQfNZOGzUByhF9Y1Bcb1QqMh0akVPgwYUdWC8xIg5ZqmWGCzaD0T5SB/exec",
+    // URL oficial de la Web App desplegada en Google Apps Script (Versión 35 Actualizada)
+    GOOGLE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwAJgAi_S0ZulK2tZRyrG5WeQfnRjU8taWZ9brO5WOKoa1wLRNM3aIUlVsxghazNh5-/exec",
     
     // Credenciales de EmailJS (para el formulario de contacto/asesoría)
     EMAILJS_SERVICE_ID: "tu_service_id",
